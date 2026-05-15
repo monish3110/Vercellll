099498ba8aa96170da129a6f6db6d05624e6283a
Monish M <monish.m@fundsindia.com>
2026-05-16 00:35:32 +0530

    bug fix

--- api/queue.py
+++ api/queue.py
@@ -1,5 +1,5 @@
 from vercel.workers import subscribe, get_asgi_app
-@subscribe("payments")
+@subscribe(topic="payments")
 def process(payload, metadata):
     print("PWNED! PAYLOAD EXECUTED:", payload)
     return True
